983703d16024499dcabf4d948af80de9a79490c2
stkener <stkener@gmail.com>
2024-04-14 20:38:02 -0300

    primero

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
+import { HashRouter as Router, Route, Switch, Link, Routes } from 'react-router-dom';
+
+import Home from './component/Home';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          
+        </Routes>
+      </div>
+    </Router> 
   );
 }
 
 export default App;
+/*
+import Barra from "./component/Barra";
+import About from "./component/About";
+import Proyectos from "./component/Proyectos";
+import Experiencia from "./component/Experiencia";
+import Educacion from "./component/Educacion";
+
+
+          <Route path="/About" element={<About />} />
+          <Route path="/Proyectos" element={<Proyectos />} />
+          <Route path="/Experiencia" element={<Experiencia />} />
+          <Route path="/Educacion" element={<Educacion />} />
+
+*/
---
+++ src/component/Home.jsx
@@ -0,0 +1,80 @@
+import React, { Fragment, useEffect, useState } from "react";
+import { BsChevronRight } from "react-icons/bs";
+import { HashRouter as Router, Route, Switch, Link } from 'react-router-dom';
+
+/*import { Container, Button, Alert } from "react-bootstrap";*/
+
+
+
+/*import About from './About.jsx';*/
+
+
+const Home = () => {
+    const [textoPresentacion, setTextoPresentacion] = useState('');
+    const textoHome = [
+        ["c", "o", "n", "s", "t", " ", "d", "e", "v", "e", "l", "o", "p", "e", "r", " ", "=", " ", "{"],
+        ["  ","f", "u", "l", "l", "N", "a", "m", "e", ":", " ","'", "S", "e", "b", "a", "s", "t", "i", "a", "n", " ", "K", "e", "n", "e", "r", "'", ","],
+        ["  ","s","e","n","i","o","r","i","t","y",":"," ","'","t","r","a","i","n","e","e","/","j","r","'",","],
+       "}"
+    ];
+
+    let contadorLetra = -1;
+    let contadorTexto = 0;
+
+    const mostrarTexto = (texto) => {
+
+        contadorLetra++;
+        
+        if (contadorTexto < texto.length) {
+            if (texto[contadorTexto] && contadorLetra < texto[contadorTexto].length) {
+                if (contadorLetra === texto[contadorTexto].length - 1) {
+                    setTextoPresentacion(prevState => prevState + texto[contadorTexto][contadorLetra] + '\n');
+                } else {
+                    setTextoPresentacion(prevState => prevState + texto[contadorTexto][contadorLetra]);
+                }
+            } else {
+                contadorTexto++;
+                contadorLetra = -1;
+            }
+        }
+    }
+    
+    useEffect(() => {
+        const timer = setInterval(() => {
+            mostrarTexto(textoHome);
+        }, 75);
+        return () => clearInterval(timer);
+    }, []);
+
+    
+    const [ocultar, setOcultar] = useState(false);
+
+    useEffect(() => {
+        localStorage.setItem('textoPresentacion', textoPresentacion);
+    }, [textoPresentacion]);
+    
+    return (
+        <Fragment>
+            <div className="contenedorPresentacion">
+                <pre>
+                    <code className="formatoLetraHome">
+                        {textoPresentacion}
+                        <span className="cursor titila">&#95;</span>
+                    </code>
+                </pre>
+            </div>
+            <Link 
+                to="/"/*About*/ 
+                className="contenedorFlecha" 
+                onMouseEnter={() => setOcultar(true)} 
+                onMouseLeave={() => setOcultar(false)}>
+                    
+                <span className={`formatoTituloFlecha ${ocultar ? "visible" : ""}`}>PORFOLIO</span>
+                <span className="formatoFlecha"><BsChevronRight /></span>
+            </Link>
+        </Fragment>
+    );
+}
+
+export default Home;
+
